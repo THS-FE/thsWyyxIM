d671193f60564816f8f103a805b62681d7d8c355
liuyx <867947704@qq.com>
2020-02-21 12:18:12 +0800

    初始化插件代码

---
+++ src/android/thsWyyxIM.java
@@ -0,0 +1,100 @@
+package cn.com.ths.wyyx.im;
+
+import android.annotation.TargetApi;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.Build;
+import android.os.PowerManager;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Pair;
+import android.view.View;
+import android.widget.Toast;
+
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.apache.cordova.CordovaWebView;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class thsWyyxIM extends CordovaPlugin {
+    public static final  String TAG = "thsWyyxIM-CordovaPlugin";
+    private static thsWyyxIM instance;
+    public thsWyyxIM() {
+        instance = this;
+    }
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        requestPermissions();
+        Log.e(TAG,"initialize");
+    }
+
+    @TargetApi(23)
+    private void requestPermissions() {
+        PermissionsManager.getInstance().requestAllManifestPermissionsIfNecessary(cordova.getActivity(), new PermissionsResultAction() {
+            @Override
+            public void onGranted() {
+//				Toast.makeText(MainActivity.this, "All permissions have been granted", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDenied(String permission) {
+                //Toast.makeText(MainActivity.this, "Permission " + permission + " has been denied", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        // 登录即时通讯服务器
+        if (action.equals("loginIM")) {
+            //userName,pwd
+            String userName = args.getString(0);
+            String pwd = args.getString(1);
+            this.loginIM( userName, pwd, callbackContext);
+            return true;
+         // 退出登录
+        }else  if(action.equals("logout")){
+            this.logout(callbackContext);
+            return true;
+        }else if(action.equals("isLoggedIn")){
+        //    boolean isLoggedIn = DemoHelper.getInstance().isLoggedIn();
+        //     callbackContext.success(isLoggedIn+"");
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 退出登录
+     */
+    private  void logout(CallbackContext callbackContext){
+        callbackContext.success("success");
+    }
+    /**
+     * 登录IM服务器
+     */
+    private void loginIM(String currentUsername, String currentPassword,CallbackContext callbackContext){
+        
+        callbackContext.success("success");
+    }
+   
+  
+}
---
+++ www/thsWyyxIM.js
@@ -0,0 +1,137 @@
+var exec = require('cordova/exec');
+/**
+ * 登录即时通讯IM的方法
+ * @param userName 用户名
+ * @param pwd 密码
+ */
+exports.loginIM = function (userName, pwd, success, error) {
+    exec(success, error, 'thsWyyxIM', 'loginIM', [userName,pwd]);
+};
+
+/**
+ *退出登录
+ */
+exports.logout = function ( success, error) {
+    exec(success, error, 'thsWyyxIM', 'logout', []);
+};
+
+/**
+ * 启动聊天界面
+ * @param toChat 聊天对象
+ * @param chattype  * 打开聊天页面，包含单聊，群聊，聊天室，讨论组(暂时不可用)，客服
+     * public static final int CHATTYPE_SINGLE = 1;
+     *     public static final int CHATTYPE_GROUP = 2;
+     *     public static final int CHATTYPE_CHATROOM = 3;
+ */
+exports.startChat = function (toChat,chattype, success, error) {
+    exec(success, error, 'thsWyyxIM', 'startChat', [toChat,chattype]);
+};
+
+/**
+ * 获取会话列表数据
+ */
+exports.getConversation = function (success, error) {
+    exec(success, error, 'thsWyyxIM', 'getConversation', []);
+};
+/**
+ * 进入添加联系人页面
+ */
+exports.startAddContact = function (success, error) {
+    exec(success, error, 'thsWyyxIM', 'startAddContact', []);
+};
+/**
+ * 进入申请与通知页面
+ */
+exports.startNewFriendsMsg = function (success, error) {
+    exec(success, error, 'thsWyyxIM', 'startNewFriendsMsg', []);
+};
+/**
+ * 进入群聊列表页面
+ */
+exports.startGroups = function (success, error) {
+    exec(success, error, 'thsWyyxIM', 'startGroups', []);
+};
+/**
+ * 进入聊天室页面
+ */
+exports.startPublicChatRooms = function (success, error) {
+    exec(success, error, 'thsWyyxIM', 'startPublicChatRooms', []);
+};
+/**
+ * 进入音视频会议页面
+ */
+exports.startConferenceCall = function (success, error) {
+    exec(success, error, 'thsWyyxIM', 'startConferenceCall', []);
+};
+/**
+ * 进入音视频会议页面
+ */
+exports.startConferenceCallByMembers = function (members,success, error) {
+    exec(success, error, 'thsWyyxIM', 'startConferenceCallByMembers', [members]);
+};
+
+/**
+ * 获取未读消息总数
+ */
+exports.getUnreadMsgCountTotal  = function (success, error) {
+    exec(success, error, 'thsWyyxIM', 'getUnreadMsgCountTotal', []);
+};
+/**
+ * 获取登录状态
+ */
+exports.isLoggedIn  = function (success, error) {
+    exec(success, error, 'thsWyyxIM', 'isLoggedIn', []);
+};
+
+exports.getCurrentUser = function(success, error){
+exec(success, error, 'thsWyyxIM', 'getCurrentUser', []);
+};
+/**
+ * 存储数据
+ */
+exports.saveContactList = function(data,success, error){
+  exec(success, error, 'thsWyyxIM', 'saveContactList', [data]);
+};
+
+/**
+ * 存储单个人数据
+ */
+exports.saveContact = function(data,success, error){
+  exec(success, error, 'thsWyyxIM', 'saveContact', [data]);
+};
+/**
+ * 删除单个人数据
+ */
+exports.deleteContact = function(data,success, error){
+  exec(success, error, 'thsWyyxIM', 'deleteContact', [data]);
+};
+
+
+
+//收到消息刷新UI界面
+exports.refreshUIWithMessageInAndroidCallback = function(data) {
+    //data = JSON.stringify(data)
+   //console.log('refreshUIWithMessageInAndroidCallback' + data);
+   cordova.fireDocumentEvent('hxim.refreshUIWithMessage', data);
+};
+// 网路状态监听回调
+exports.onDisconnectedReceiverInAndroidCallback = function(data) {
+    data = JSON.stringify(data);
+    data = JSON.parse(data);
+   //console.log('onDisconnectedReceiverInAndroidCallback' + data);
+   cordova.fireDocumentEvent('hxim.onDisconnectedReceiver', data);
+};
+// 联系人更新
+exports.contactUpdateInAndroidCallback = function(data) {
+   //data = JSON.stringify(data)
+  // console.log('contactUpdateInAndroidCallback' + data);
+   cordova.fireDocumentEvent('hxim.contactUpdate', data);
+};
+
+
+
+
+
+
+
+
